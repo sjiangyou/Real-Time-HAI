a83e90e5f96daa9781e8ab88f543421d4dd2235c
Sunny You <sunny.jiang.you@gmail.com>
2025-07-23 00:03:36 -0400

    Included a validation dataset for HIECNN training.

--- HIECNN/CODE/Model_Prep.R
+++ HIECNN/CODE/Model_Prep.R
@@ -4,16 +4,23 @@ setwd(here())
 options(scipen = 999)
 
 main <- function() {
+    validation_end <- 2017
+    validation_end <- as.numeric(paste0(validation_end, "000000"))
     train_end <- 2018
     train_end <- as.numeric(paste0(train_end, "000000"))
 
     all_data <- process_data()
-    train <- all_data[all_data$DATE < train_end, ]
+    train <- all_data[all_data$DATE < validation_end, ]
+    validation <- all_data[all_data$DATE >= validation_end & all_data$DATE < train_end, ]
     test <- all_data[all_data$DATE >= train_end, ]
 
     train$CAT <- ifelse(train$VMAX <= 33, "TD",
         ifelse(train$VMAX <= 63, "TS",
             ifelse(train$VMAX <= 95, "Min", "Maj")))
+
+    validation$CAT <- ifelse(validation$VMAX <= 33, "TD",
+        ifelse(validation$VMAX <= 63, "TS",
+            ifelse(validation$VMAX <= 95, "Min", "Maj")))
 
     test$CAT <- ifelse(test$VMAX <= 33, "TD",
         ifelse(test$VMAX <= 63, "TS",
@@ -24,11 +31,13 @@ main <- function() {
             function(y) doresample(train, y, max(table(train$CAT)))))
 
     print(paste0("Training data size: ", nrow(train)))
+    print(paste0("Validation data size: ", nrow(validation)))
     print(paste0("Testing data size: ", nrow(test)))
 
     dir.create("HIECNN/IMERG/DEV", recursive = TRUE, showWarnings = FALSE)
 
     write.csv(train, "HIECNN/IMERG/DEV/ALL_TRAIN_DATA.csv", row.names = FALSE)
+    write.csv(validation, "HIECNN/IMERG/DEV/ALL_VALIDATION_DATA.csv", row.names = FALSE)
     write.csv(test, "HIECNN/IMERG/DEV/ALL_TEST_DATA.csv", row.names = FALSE)
     write.csv(train_resample, "HIECNN/IMERG/DEV/ALL_TRAIN_DATA_RESAMPLE.csv", row.names = FALSE)
 }
