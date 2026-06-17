b1046bbf39b5939a1f23ab327d2655d0e8534e51
Sunny You <sunny.jiang.you@gmail.com>
2026-06-17 17:47:05 -0500

    Changed name of code directories.

--- HCNN/src/TC_Prediction.py
+++ HCNN/src/TC_Prediction.py
@@ -9,22 +9,23 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import tensorflow as tf 
+import tensorflow as tf
 from tensorflow import keras
 import shap
-
 
 # In[9]:
 
 
-os.chdir('/Users/sunnyyou/Documents/Real_Time_HAI/HIPCNN/IMERG')
+os.chdir("/Users/sunnyyou/Documents/Real_Time_HAI/HIPCNN/IMERG")
 
 
 # In[181]:
 
 
 train = pd.read_csv("DEV/P06_2018_train_resample.csv")
-train = train[["GIS_ID", "DATE", "SHIPS_PER", "SHIPS_POT", "VMAX", "SHDC", "IC", "Category"]]
+train = train[
+    ["GIS_ID", "DATE", "SHIPS_PER", "SHIPS_POT", "VMAX", "SHDC", "IC", "Category"]
+]
 # train = train.drop(["VMAX_FT"], axis = 1)
 train.columns = ["GIS_ID", "DATE", "PER", "POT", "VMAX", "SHDC_FT", "IC", "Category"]
 train
@@ -34,7 +35,9 @@ train
 
 
 test = pd.read_csv("DEV/P06_2018_test.csv")
-test = test[["GIS_ID", "DATE", "SHIPS_PER", "SHIPS_POT", "VMAX", "SHDC", "IC", "Category"]]
+test = test[
+    ["GIS_ID", "DATE", "SHIPS_PER", "SHIPS_POT", "VMAX", "SHDC", "IC", "Category"]
+]
 # test = test.drop(["VMAX_FT"], axis = 1)
 test.columns = ["GIS_ID", "DATE", "PER", "POT", "VMAX", "SHDC_FT", "IC", "Category"]
 test
@@ -61,11 +64,11 @@ train_label = []
 test_img = []
 test_ships = []
 test_label = []
-for f in range(len(train.GIS_ID)) :
+for f in range(len(train.GIS_ID)):
     filename = "IMERG_CSV/" + train.GIS_ID[f] + ".csv"
     try:
-        temp = pd.read_csv(filename, header = None)
-        if (temp.shape != (121,121)):
+        temp = pd.read_csv(filename, header=None)
+        if temp.shape != (121, 121):
             continue
         temp = temp[40:81]
         temp = temp.iloc[:, 40:81]
@@ -78,11 +81,11 @@ for f in range(len(train.GIS_ID)) :
     except Exception as e:
         pass
 
-for f in range(len(test.GIS_ID)) :
+for f in range(len(test.GIS_ID)):
     filename = "IMERG_CSV/" + test.GIS_ID[f] + ".csv"
     try:
-        temp = pd.read_csv(filename, header = None)
-        if (temp.shape != (121,121)):
+        temp = pd.read_csv(filename, header=None)
+        if temp.shape != (121, 121):
             continue
         temp = temp[40:81]
         temp = temp.iloc[:, 40:81]
@@ -129,10 +132,10 @@ y_test = test_label
 
 
 X_train_img = np.array(X_train_img)
-X_train_img = X_train_img.reshape(-1,41,41,1)
-X_train_img = X_train_img.astype('float32')
+X_train_img = X_train_img.reshape(-1, 41, 41, 1)
+X_train_img = X_train_img.astype("float32")
 X_train_ships = np.array(X_train_ships)
-X_train_ships = X_train_ships.reshape(-1,4)
+X_train_ships = X_train_ships.reshape(-1, 4)
 y_train = np.array(y_train)
 
 
@@ -140,10 +143,10 @@ y_train = np.array(y_train)
 
 
 X_test_img = np.array(X_test_img)
-X_test_img = X_test_img.reshape(-1,41,41,1)
-X_test_img = X_test_img.astype('float32')
+X_test_img = X_test_img.reshape(-1, 41, 41, 1)
+X_test_img = X_test_img.astype("float32")
 X_test_ships = np.array(X_test_ships)
-X_test_ships = X_test_ships.reshape(-1,4)
+X_test_ships = X_test_ships.reshape(-1, 4)
 y_test = np.array(y_test)
 
 
@@ -157,18 +160,18 @@ print(X_train_ships.shape)
 # In[136]:
 
 
-ships_input = keras.Input(shape =(4,), name = "ships_layer")
-img_input = keras.Input(shape =(41, 41, 1), name = "img_layer")
+ships_input = keras.Input(shape=(4,), name="ships_layer")
+img_input = keras.Input(shape=(41, 41, 1), name="img_layer")
 
-w = keras.layers.Conv2D(64,8) (img_input)
-w = keras.layers.Conv2D(64,8)(w)
-w = keras.layers.Conv2D(64,1)(w)
+w = keras.layers.Conv2D(64, 8)(img_input)
+w = keras.layers.Conv2D(64, 8)(w)
+w = keras.layers.Conv2D(64, 1)(w)
 w = keras.layers.BatchNormalization()(w)
 w = keras.activations.relu(w)
-w = keras.layers.MaxPool2D(2,2)(w)
+w = keras.layers.MaxPool2D(2, 2)(w)
 w = keras.layers.Conv2D(64, 3)(w)
 w = keras.layers.Conv2D(64, 3)(w)
-w = keras.layers.Conv2D(256,1)(w)
+w = keras.layers.Conv2D(256, 1)(w)
 w = keras.layers.BatchNormalization()(w)
 img_output1 = keras.layers.Flatten()(w)
 
@@ -176,63 +179,71 @@ merged_model1 = keras.layers.concatenate([img_output1, ships_input])
 output_layer1 = keras.layers.Dense(256)(merged_model1)
 output_layer1 = keras.layers.Dense(165)(output_layer1)
 
-new_model1 = keras.Model(inputs = [img_input, ships_input], outputs = output_layer1, name = "model_1")
+new_model1 = keras.Model(
+    inputs=[img_input, ships_input], outputs=output_layer1, name="model_1"
+)
 
 new_model1.summary()
 
-x = keras.layers.Conv2D(256, 8) (img_input)
+x = keras.layers.Conv2D(256, 8)(img_input)
 x = keras.layers.BatchNormalization()(x)
 x = keras.activations.relu(x)
-x = keras.layers.MaxPool2D(2,2)(x)
-x = keras.layers.Conv2D(128,1, activation = 'relu')(x)
-x = keras.layers.Conv2D(128,5)(x)
+x = keras.layers.MaxPool2D(2, 2)(x)
+x = keras.layers.Conv2D(128, 1, activation="relu")(x)
+x = keras.layers.Conv2D(128, 5)(x)
 x = keras.layers.BatchNormalization()(x)
 x = keras.activations.relu(x)
-x = keras.layers.MaxPool2D(2,2)(x)
-x = keras.layers.Conv2D(64,1, activation = 'relu')(x)
-x = keras.layers.Conv2D(64,3)(x)
+x = keras.layers.MaxPool2D(2, 2)(x)
+x = keras.layers.Conv2D(64, 1, activation="relu")(x)
+x = keras.layers.Conv2D(64, 3)(x)
 x = keras.layers.BatchNormalization()(x)
 x = keras.activations.relu(x)
-x = keras.layers.MaxPool2D(2,2)(x)
+x = keras.layers.MaxPool2D(2, 2)(x)
 img_output2 = keras.layers.Flatten()(x)
 
 merged_model2 = keras.layers.concatenate([img_output2, ships_input])
 output_layer2 = keras.layers.Dense(165)(merged_model2)
 
-new_model2 = keras.Model(inputs = [img_input, ships_input], outputs = output_layer2, name = "model_2")
+new_model2 = keras.Model(
+    inputs=[img_input, ships_input], outputs=output_layer2, name="model_2"
+)
 
 new_model2.summary()
 
-y = keras.layers.Conv2D(256, (6,2))(img_input)
-y = keras.layers.Conv2D(256, (2,6))(y)
+y = keras.layers.Conv2D(256, (6, 2))(img_input)
+y = keras.layers.Conv2D(256, (2, 6))(y)
 y = keras.layers.BatchNormalization()(y)
 y = keras.activations.relu(y)
-y = keras.layers.MaxPool2D(2,2)(y)
-y = keras.layers.Conv2D(128,4)(y)
+y = keras.layers.MaxPool2D(2, 2)(y)
+y = keras.layers.Conv2D(128, 4)(y)
 y = keras.layers.BatchNormalization()(y)
 y = keras.activations.relu(y)
-y = keras.layers.MaxPool2D(2,2)(y)
-y = keras.layers.Conv2D(64,3)(y)
+y = keras.layers.MaxPool2D(2, 2)(y)
+y = keras.layers.Conv2D(64, 3)(y)
 y = keras.layers.BatchNormalization()(y)
 y = keras.activations.relu(y)
-y = keras.layers.MaxPool2D(2,2)(y)
+y = keras.layers.MaxPool2D(2, 2)(y)
 img_output3 = keras.layers.Flatten()(y)
 
 merged_model3 = keras.layers.concatenate([img_output3, ships_input])
 output_layer3 = keras.layers.Dense(165)(merged_model3)
 
-new_model3 = keras.Model(inputs = [img_input, ships_input], outputs = output_layer3, name = "model_3")
+new_model3 = keras.Model(
+    inputs=[img_input, ships_input], outputs=output_layer3, name="model_3"
+)
 
 new_model3.summary()
 
 z = keras.layers.Conv2D(256, 7)(img_input)
-z = keras.layers.MaxPool2D(2,2)(z)
+z = keras.layers.MaxPool2D(2, 2)(z)
 img_output4 = keras.layers.Flatten()(z)
 
 merged_model4 = keras.layers.concatenate([img_output4, ships_input])
 output_layer4 = keras.layers.Dense(165)(merged_model4)
 
-new_model4 = keras.Model(inputs = [img_input, ships_input], outputs = output_layer4, name = "model_4")
+new_model4 = keras.Model(
+    inputs=[img_input, ships_input], outputs=output_layer4, name="model_4"
+)
 
 new_model4.summary()
 
@@ -241,17 +252,17 @@ new_model4.summary()
 
 
 # ships_input = keras.Input(shape =(4,), name = "ships_layer")
-img_input = keras.Input(shape =(41, 41, 1), name = "img_layer")
+img_input = keras.Input(shape=(41, 41, 1), name="img_layer")
 
-w = keras.layers.Conv2D(64,8) (img_input)
-w = keras.layers.Conv2D(64,8)(w)
-w = keras.layers.Conv2D(64,1)(w)
+w = keras.layers.Conv2D(64, 8)(img_input)
+w = keras.layers.Conv2D(64, 8)(w)
+w = keras.layers.Conv2D(64, 1)(w)
 w = keras.layers.BatchNormalization()(w)
 w = keras.activations.relu(w)
-w = keras.layers.MaxPool2D(2,2)(w)
+w = keras.layers.MaxPool2D(2, 2)(w)
 w = keras.layers.Conv2D(64, 3)(w)
 w = keras.layers.Conv2D(64, 3)(w)
-w = keras.layers.Conv2D(256,1)(w)
+w = keras.layers.Conv2D(256, 1)(w)
 w = keras.layers.BatchNormalization()(w)
 img_output1 = keras.layers.Flatten()(w)
 
@@ -259,63 +270,63 @@ merged_model1 = keras.layers.concatenate([img_output1])
 output_layer1 = keras.layers.Dense(256)(merged_model1)
 output_layer1 = keras.layers.Dense(165)(output_layer1)
 
-new_model1 = keras.Model(inputs = [img_input], outputs = output_layer1, name = "model_1")
+new_model1 = keras.Model(inputs=[img_input], outputs=output_layer1, name="model_1")
 
 new_model1.summary()
 
-x = keras.layers.Conv2D(256, 8) (img_input)
+x = keras.layers.Conv2D(256, 8)(img_input)
 x = keras.layers.BatchNormalization()(x)
 x = keras.activations.relu(x)
-x = keras.layers.MaxPool2D(2,2)(x)
-x = keras.layers.Conv2D(128,1, activation = 'relu')(x)
-x = keras.layers.Conv2D(128,5)(x)
+x = keras.layers.MaxPool2D(2, 2)(x)
+x = keras.layers.Conv2D(128, 1, activation="relu")(x)
+x = keras.layers.Conv2D(128, 5)(x)
 x = keras.layers.BatchNormalization()(x)
 x = keras.activations.relu(x)
-x = keras.layers.MaxPool2D(2,2)(x)
-x = keras.layers.Conv2D(64,1, activation = 'relu')(x)
-x = keras.layers.Conv2D(64,3)(x)
+x = keras.layers.MaxPool2D(2, 2)(x)
+x = keras.layers.Conv2D(64, 1, activation="relu")(x)
+x = keras.layers.Conv2D(64, 3)(x)
 x = keras.layers.BatchNormalization()(x)
 x = keras.activations.relu(x)
-x = keras.layers.MaxPool2D(2,2)(x)
+x = keras.layers.MaxPool2D(2, 2)(x)
 img_output2 = keras.layers.Flatten()(x)
 
 merged_model2 = keras.layers.concatenate([img_output2])
 output_layer2 = keras.layers.Dense(165)(merged_model2)
 
-new_model2 = keras.Model(inputs = [img_input], outputs = output_layer2, name = "model_2")
+new_model2 = keras.Model(inputs=[img_input], outputs=output_layer2, name="model_2")
 
 new_model2.summary()
 
-y = keras.layers.Conv2D(256, (6,2))(img_input)
-y = keras.layers.Conv2D(256, (2,6))(y)
+y = keras.layers.Conv2D(256, (6, 2))(img_input)
+y = keras.layers.Conv2D(256, (2, 6))(y)
 y = keras.layers.BatchNormalization()(y)
 y = keras.activations.relu(y)
-y = keras.layers.MaxPool2D(2,2)(y)
-y = keras.layers.Conv2D(128,4)(y)
+y = keras.layers.MaxPool2D(2, 2)(y)
+y = keras.layers.Conv2D(128, 4)(y)
 y = keras.layers.BatchNormalization()(y)
 y = keras.activations.relu(y)
-y = keras.layers.MaxPool2D(2,2)(y)
-y = keras.layers.Conv2D(64,3)(y)
+y = keras.layers.MaxPool2D(2, 2)(y)
+y = keras.layers.Conv2D(64, 3)(y)
 y = keras.layers.BatchNormalization()(y)
 y = keras.activations.relu(y)
-y = keras.layers.MaxPool2D(2,2)(y)
+y = keras.layers.MaxPool2D(2, 2)(y)
 img_output3 = keras.layers.Flatten()(y)
 
 merged_model3 = keras.layers.concatenate([img_output3])
 output_layer3 = keras.layers.Dense(165)(merged_model3)
 
-new_model3 = keras.Model(inputs = [img_input], outputs = output_layer3, name = "model_3")
+new_model3 = keras.Model(inputs=[img_input], outputs=output_layer3, name="model_3")
 
 new_model3.summary()
 
 z = keras.layers.Conv2D(256, 7)(img_input)
-z = keras.layers.MaxPool2D(2,2)(z)
+z = keras.layers.MaxPool2D(2, 2)(z)
 img_output4 = keras.layers.Flatten()(z)
 
 merged_model4 = keras.layers.concatenate([img_output4])
 output_layer4 = keras.layers.Dense(165)(merged_model4)
 
-new_model4 = keras.Model(inputs = [img_input], outputs = output_layer4, name = "model_4")
+new_model4 = keras.Model(inputs=[img_input], outputs=output_layer4, name="model_4")
 
 new_model4.summary()
 
@@ -323,60 +334,80 @@ new_model4.summary()
 # In[137]:
 
 
-new_model1.compile(optimizer='adam',
-              loss=tf.keras.losses.MeanAbsoluteError(),
-              metrics=['mae', 'mse'])
-new_model2.compile(optimizer='adam',
-              loss=tf.keras.losses.MeanAbsoluteError(),
-              metrics=['mae', 'mse'])
-new_model3.compile(optimizer='adam',
-              loss=tf.keras.losses.MeanAbsoluteError(),
-              metrics=['mae', 'mse'])
-new_model4.compile(optimizer='adam',
-              loss=tf.keras.losses.MeanAbsoluteError(),
-              metrics=['mae', 'mse'])
+new_model1.compile(
+    optimizer="adam", loss=tf.keras.losses.MeanAbsoluteError(), metrics=["mae", "mse"]
+)
+new_model2.compile(
+    optimizer="adam", loss=tf.keras.losses.MeanAbsoluteError(), metrics=["mae", "mse"]
+)
+new_model3.compile(
+    optimizer="adam", loss=tf.keras.losses.MeanAbsoluteError(), metrics=["mae", "mse"]
+)
+new_model4.compile(
+    optimizer="adam", loss=tf.keras.losses.MeanAbsoluteError(), metrics=["mae", "mse"]
+)
 
 
 # In[138]:
 
 
-new_model3.fit([X_train_img, X_train_ships], y_train, epochs=3, batch_size=8, validation_split = 0.1)
+new_model3.fit(
+    [X_train_img, X_train_ships], y_train, epochs=3, batch_size=8, validation_split=0.1
+)
 res = new_model3.evaluate([X_test_img, X_test_ships], y_test)
 print("MAE = " + str(res[0]))
 print("RMSE = " + str((res[2]) ** 0.5))
 preds = new_model3.predict([X_test_img, X_test_ships])
-a = np.average(preds, axis = 1)
-test['preds'] = a
+a = np.average(preds, axis=1)
+test["preds"] = a
 
 
 # In[10]:
 
 
 train_no_resample = pd.read_csv("DEV/P06_2018_train.csv")
-train_no_resample = train_no_resample[["GIS_ID", "DATE", "SHIPS_PER", "SHIPS_POT", "VMAX", "SHDC", "IC", "Category"]]
-train_no_resample.columns = ["GIS_ID", "DATE", "PER", "POT", "VMAX", "SHDC_FT", "IC", "Category"]
+train_no_resample = train_no_resample[
+    ["GIS_ID", "DATE", "SHIPS_PER", "SHIPS_POT", "VMAX", "SHDC", "IC", "Category"]
+]
+train_no_resample.columns = [
+    "GIS_ID",
+    "DATE",
+    "PER",
+    "POT",
+    "VMAX",
+    "SHDC_FT",
+    "IC",
+    "Category",
+]
 train_no_resample
 
 
 # In[141]:
 
 
-train_no_resample['Category'].value_counts()['Maj']
+train_no_resample["Category"].value_counts()["Maj"]
 
 
 # In[142]:
 
 
-train_no_resample_TD = train_no_resample[train_no_resample['Category'] == 'TD']
+train_no_resample_TD = train_no_resample[train_no_resample["Category"] == "TD"]
 train_no_resample_TD = train_no_resample_TD.sample(90)
-train_no_resample_TS = train_no_resample[train_no_resample['Category'] == 'TS']
+train_no_resample_TS = train_no_resample[train_no_resample["Category"] == "TS"]
 train_no_resample_TS = train_no_resample_TS.sample(90)
-train_no_resample_Min = train_no_resample[train_no_resample['Category'] == 'Min']
+train_no_resample_Min = train_no_resample[train_no_resample["Category"] == "Min"]
 train_no_resample_Min = train_no_resample_Min.sample(90)
-train_no_resample_Maj = train_no_resample[train_no_resample['Category'] == 'Maj']
+train_no_resample_Maj = train_no_resample[train_no_resample["Category"] == "Maj"]
 train_no_resample_Maj = train_no_resample_Maj.sample(90)
-shap_train = pd.concat([train_no_resample_TD, train_no_resample_TS, train_no_resample_Min, train_no_resample_Maj])
-shap_train = shap_train.reset_index(drop = True)
+shap_train = pd.concat(
+    [
+        train_no_resample_TD,
+        train_no_resample_TS,
+        train_no_resample_Min,
+        train_no_resample_Maj,
+    ]
+)
+shap_train = shap_train.reset_index(drop=True)
 shap_train
 
 
@@ -386,11 +417,11 @@ shap_train
 shap_train_img = []
 shap_train_ships = []
 shap_train_label = []
-for f in range(len(shap_train.GIS_ID)) :
+for f in range(len(shap_train.GIS_ID)):
     filename = "IMERG_CSV/" + shap_train.GIS_ID[f] + ".csv"
     try:
-        temp = pd.read_csv(filename, header = None)
-        if (temp.shape != (121,121)):
+        temp = pd.read_csv(filename, header=None)
+        if temp.shape != (121, 121):
             continue
         temp = temp[40:81]
         temp = temp.iloc[:, 40:81]
@@ -398,7 +429,14 @@ for f in range(len(shap_train.GIS_ID)) :
         shap_train_img.append(temp)
         lab = shap_train.IC[f] + shap_train.VMAX[f]
         shap_train_label.append(lab)
-        ships = np.array([shap_train.VMAX[f], shap_train.POT[f], shap_train.PER[f], shap_train.SHDC_FT[f]])
+        ships = np.array(
+            [
+                shap_train.VMAX[f],
+                shap_train.POT[f],
+                shap_train.PER[f],
+                shap_train.SHDC_FT[f],
+            ]
+        )
         shap_train_ships.append(ships)
     except Exception as e:
         pass
@@ -408,10 +446,10 @@ for f in range(len(shap_train.GIS_ID)) :
 
 
 shap_train_img = np.array(shap_train_img)
-shap_train_img = shap_train_img.reshape(-1,41,41,1)
-shap_train_img = shap_train_img.astype('float32')
+shap_train_img = shap_train_img.reshape(-1, 41, 41, 1)
+shap_train_img = shap_train_img.astype("float32")
 shap_train_ships = np.array(shap_train_ships)
-shap_train_ships = shap_train_ships.reshape(-1,4)
+shap_train_ships = shap_train_ships.reshape(-1, 4)
 shap_train_label = np.array(shap_train_label)
 
 
@@ -424,8 +462,10 @@ len(shap_train_label)
 # In[146]:
 
 
-#shap.explainers._deep.deep_tf.op_handlers["AddV2"] = shap.explainers._deep.deep_tf.passthrough
-shap.explainers._deep.deep_tf.op_handlers["FusedBatchNormV3"] = shap.explainers._deep.deep_tf.linearity_1d(0)
+# shap.explainers._deep.deep_tf.op_handlers["AddV2"] = shap.explainers._deep.deep_tf.passthrough
+shap.explainers._deep.deep_tf.op_handlers["FusedBatchNormV3"] = (
+    shap.explainers._deep.deep_tf.linearity_1d(0)
+)
 e = shap.DeepExplainer(new_model3, [shap_train_img, shap_train_ships])
 shap_values = e.shap_values([X_test_img, X_test_ships])
 # shap.summary_plot(shap_values, X_test_img)
@@ -505,7 +545,7 @@ np.sum(shap_b)
 # In[31]:
 
 
-(np.sum(np.sum(np.abs(shap_values[0][0][0]), axis = 0), axis=0))
+(np.sum(np.sum(np.abs(shap_values[0][0][0]), axis=0), axis=0))
 
 
 # In[30]:
@@ -536,7 +576,3 @@ e_nov21 = e
 
 
 # In[ ]:
-
-
-
-
